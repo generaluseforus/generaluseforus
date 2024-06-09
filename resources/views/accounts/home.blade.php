514157a9f0e3fcad08a8c554ab9f80076cc08603
generaluserforus <generaluseforus@gmail.com>
2024-06-09 11:53:11 +0530

    expense crud done

--- resources/views/accounts/home.blade.php
+++ resources/views/accounts/home.blade.php
@@ -6,6 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
+                <!-- <div class="card-header">{{ $userData->name }}</div> -->
 
                 <div class="card-body">
                     @if (session('status'))
